308af3642ddf2df5f190bb3a798c6b7e0c1f90c4
Mamunur1093 <mdmamunur1102@gmail.com>
2026-01-22 11:55:34 +0600

    Installed react.js with tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl text-center font-bold'>Hello CodeSandbox</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
